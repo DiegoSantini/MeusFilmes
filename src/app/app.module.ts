d774ceab4d40d6234b403079451f37821385f8c0
DiegoSantini <didisantini@gmail.com>
2018-10-11 10:52:00 -0300

    Versão Aula 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { CadastrarPage } from '../pages/cadastrar/cadastrar';
+import { GeneroPage } from '../pages/genero/genero';
+import { LancamentosPage } from '../pages/lancamentos/lancamentos';
+import { PopularidadePage } from '../pages/popularidade/popularidade';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPageModule } from '../pages/intro/intro.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    CadastrarPage,
+    GeneroPage,
+    LancamentosPage,
+    PopularidadePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IntroPageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    CadastrarPage,
+    GeneroPage,
+    LancamentosPage,
+    PopularidadePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
