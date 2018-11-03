603c8042160c2e8386a0198fb81c6753bfeb363e
Diego Silva Santini dos Santos <didisantini@gmail.com>
2018-11-03 11:36:06 -0300

    Aula 08- Ionic SQLite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,62 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { CadastrarPage } from '../pages/cadastrar/cadastrar';
-
+import {HomePage} from '../pages/home/home';
 import { LancamentosPage } from '../pages/lancamentos/lancamentos';
 import{PopularidadePage} from '../pages/popularidade/popularidade'
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { IntroPageModule } from '../pages/intro/intro.module';
 import { GeneroPageModule } from '../pages/genero/genero.module';
-import { DestinoPageModule } from '../pages/destino/destino.module';
-import { FilmePageModule } from '../pages/filme/filme.module';
-
+import {HttpModule} from "@angular/http";
+import { MovieProvider } from '../providers/movie/movie';
+import {SQLite} from '@ionic-native/sqlite';
+import { DatabaseProvider } from '../providers/database/database';
+import { FilmeProvider } from '../providers/filme/filme';
+import { AdicionaFilmePage } from '../pages/adiciona-filme/adiciona-filme';
+import { ExibeFilmePage } from '../pages/exibe-filme/exibe-filme';
+import { AtualizaFilmePage } from '../pages/atualiza-filme/atualiza-filme';
 
 @NgModule({
   declarations: [
     MyApp,
-    CadastrarPage,
+    HomePage,
     PopularidadePage,
     LancamentosPage,
-    TabsPage
+    TabsPage,
+    AdicionaFilmePage,
+    ExibeFilmePage,
+    AtualizaFilmePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     IntroPageModule,
     GeneroPageModule,
-    FilmePageModule,
-    DestinoPageModule
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    CadastrarPage,
+    HomePage,
     PopularidadePage,
     LancamentosPage,
-    TabsPage
+    TabsPage,
+    AdicionaFilmePage,
+    ExibeFilmePage,
+    AtualizaFilmePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MovieProvider,
+    SQLite,
+    DatabaseProvider,
+    FilmeProvider,
+   
   ]
 })
 export class AppModule {}
